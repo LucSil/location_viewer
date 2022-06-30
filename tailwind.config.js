a4b8883cfe4fec9b1be49e1d95aadf8b1fd70b68
LucSil <luciansilva351@gmail.com>
2022-06-30 21:50:26 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./src/components/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        nunito: ["Nunito Sans", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
