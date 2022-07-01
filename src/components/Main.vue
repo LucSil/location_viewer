bbd21e10304c235ccf4b2a68678c9a56377103d9
LucSil <luciansilva351@gmail.com>
2022-07-01 12:22:37 +0100

    span tags for tooltips

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -6,10 +6,18 @@
     </div>
 
     <!-- Cameras -->
-    <button class="camera one">01</button>
-    <button class="camera two">02</button>
-    <button class="camera three">03</button>
-    <button class="camera four">04</button>
+    <button class="camera one group">
+      01<span class="tooltip tip-one">View 01</span>
+    </button>
+    <button class="camera two grou[">
+      02<span class="tooltip tip-two">View 02</span>
+    </button>
+    <button class="camera three group">
+      03<span class="tooltip tip-three">View 03</span>
+    </button>
+    <button class="camera four group">
+      04<span class="tooltip tip-four">View 04</span>
+    </button>
   </section>
 </template>
 
