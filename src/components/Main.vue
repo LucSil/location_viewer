0bb03238ce5b983a3a9a6462ec81e14a3aa3e342
LucSil <luciansilva351@gmail.com>
2022-07-01 14:29:15 +0100

    camera view-01 being implemented

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -6,15 +6,23 @@
     </div>
 
     <!-- Cameras -->
+    <!-- camera 01 -->
     <button class="camera one group">
       01<span class="tooltip tip-one">View 01</span>
     </button>
+    <div class="view01"></div>
+
+    <!-- camera 02 -->
     <button class="camera two group">
       02<span class="tooltip tip-two">View 02</span>
     </button>
+
+    <!-- camera 03 -->
     <button class="camera three group">
       03<span class="tooltip tip-three">View 03</span>
     </button>
+
+    <!-- camera 04 -->
     <button class="camera four group">
       04<span class="tooltip tip-four">View 04</span>
     </button>
@@ -26,3 +34,11 @@ export default {
   name: "Main",
 };
 </script>
+
+<style>
+.view01 {
+  background: black;
+  opacity: 0.5;
+  clip-path: polygon(0 0, 100% 0, 100% 56%, 76% 34%, 62.3% 100%, 0 100%);
+}
+</style>
