39f8fe160237fbc1d7407253e935291fa3da7487
LucSil <luciansilva351@gmail.com>
2022-07-01 07:43:09 +0100

    templating and styling for Main.vue

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,4 +1,17 @@
-<template></template>
+<template>
+  <section class="grid place-items-center relative">
+    <!-- Map Container -->
+    <div class="w-full h-full">
+      <img src="../images/map.png" alt="building-map" />
+    </div>
+
+    <!-- Cameras -->
+    <button class="camera">01</button>
+    <button class="camera">02</button>
+    <button class="camera">03</button>
+    <button class="camera">04</button>
+  </section>
+</template>
 
 <script>
 export default {
