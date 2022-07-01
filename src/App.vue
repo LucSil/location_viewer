94f50306f9debf4d294bea5abc66a79020e821ab
LucSil <luciansilva351@gmail.com>
2022-07-01 07:30:22 +0100

    imported Main.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
-<template></template>
+<template>
+  <Main />
+</template>
 
 <script>
+import Main from "@/components/Main.vue";
+
 export default {
   name: "App",
+  components: { Main },
 };
 </script>
